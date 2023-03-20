2e884c49b0fc51b77b0bc736285d84f77c926593
Mohamed Barreh <mohamed.barreh@ivts.co.uk>
2023-03-20 12:48:48 +0000

    routes for product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,26 +5,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // find all productS
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include:[
+      {
+        model: Category,
+        attributes: ['category_name']
+      }]
+  })
+  .then(data => res.json(data))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where:{
+      id: req.params.id
+    },
+    attributes: ['product_name', 'price', 'stock'],
+    include:[
+      {
+        model: Category,
+        attributes: ['category_name']
+      }]
+  })
+  .then(data => {
+    if (!data
+) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+    res.json(data
+);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -91,6 +118,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data){
+      res.status(404).json({message: 'Product not found'});
+      return;
+    }
+    res.json(data
+);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
