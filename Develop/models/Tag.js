2115105973b5b3b6c12e3689a075df0f18dd6b36
shtuupid <mohamedbarreh8@gmail.com>
2023-06-04 08:08:57 +0100

    fixes

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -11,10 +11,10 @@ Tag.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
+    },
       tag_name: {
         type: DataTypes.STRING,
-      }
-    }
+      },
   },
   {
     sequelize,
